ed52e88471af1290f87c1f931feca877bd03f682
Vu Tran <vutran@dropbox.com>
2018-01-20 09:38:09 -0800

    fix docblock

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,8 +1,8 @@
 /**
  * Returns a simple time string for a given Date instance
  *
- * @param {Date} d
- * @return {String}
+ * @param Date d
+ * @return string
  */
 export function toTime(d: Date): string {
     return d.getHours() + ":" + d.getMinutes();
